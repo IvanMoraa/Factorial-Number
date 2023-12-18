091c2a49b73452b185fbd457d4073a7879e2209c
Ivan Mora <151114916+Mworaaa@users.noreply.github.com>
2023-12-18 09:36:08 -0600

    Factorial-Numbers

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try{
+        Console.WriteLine("Write a Number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"Factorial from {number} is: {factorial}");
+        }catch(Exception ex)
+        {
+            System.Console.WriteLine($"Something went wrong: {ex}");
+        }
+    }
+}
